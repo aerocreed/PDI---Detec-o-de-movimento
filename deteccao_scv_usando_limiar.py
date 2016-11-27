42a76a1ca9672b26d20b0eff6550de17a4981d27
aerocreed <aerocreed@users.noreply.github.com>
2016-11-27 19:24:06 -0300

    quebrando a imagem em partes

--- deteccao_scv_usando_limiar.py
+++ deteccao_scv_usando_limiar.py
@@ -1,24 +1,30 @@
 from SimpleCV import *
 
 cam = Camera()
-threshold = 5.0 # if mean exceeds this amount do something
+threshold = 5.0 # caso a diferença seja maior que o limiar, o movimento será considerado
 
 cont = 0
-fps = 15
+fps = 2
+width = 320
+height = 240
 
 while True:
-	previous = cam.getImage() #grab a frame
-	time.sleep(1.0/fps) #wait for half a second
-	current = cam.getImage() #grab another frame
-	diff = current - previous
-	matrix = diff.getNumpy()
-	mean = matrix.mean()	
-	if mean >= threshold:
-		print (diff)
-		cont = cont + 1
-		pathAtual = 'atual%d.jpg' % (cont,)
-		pathAnterior = 'anterior%d.jpg' % (cont,)
-		previous.save(pathAnterior)
-		current.save(pathAtual)
-		print 'Motion Detected'
-
+	antes = cam.getImage() #captura um frame
+	time.sleep(1.0/(fps*2)) #espera uma parcela de segundo 
+	depois = cam.getImage() #captura outro frame
+	antesLst = []
+	depoisLst = []
+	difLst = []
+	depois.show()
+	i = 0
+	j = 0
+	for i in range(0,2):
+		for j in range(0,2):
+			antesLst.append(antes.crop(j*width, i*height, width, height, False))
+			depoisLst.append(depois.crop(j*width, i*height, width, height, False))
+			difLst.append(depoisLst[i*2+j] - antesLst[i*2+j])
+			matrix = difLst[i*2+j].getNumpy()
+			mean = matrix.mean()
+			if mean >= threshold:	
+				depois.drawText('Movimento detectado')
+				depois.show()		
